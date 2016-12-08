c62407496714f5eb3098753deeb6af7aa6bff8d9
Jesse Emery <jesse@jesseemery.com>
2016-12-08 14:17:58 -0500

    Refactor to make testing easier

---
+++ tests/test_settings.py
@@ -0,0 +1,20 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+MOCK_SLACK=True
+SLACKBOT_API_TOKEN='testtoken'
+SECRET_KEY = 'fake-key'
+INSTALLED_APPS = [
+    "tests",
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
